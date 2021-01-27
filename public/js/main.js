20d0b03d29b2d2a36c32a1db1fbbc1ef1d75bd8a
M. Firhan Azmi Nor <muhammad.firhan.azmi@gmail.com>
2021-01-27 23:52:08 +0700

    add automatic scroll down every new chat

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,12 @@
 const chatForm = document.getElementById('chat-form')
+const chatMessages = document.querySelector('.chat-message')
 
 const socket = io()
 
 socket.on('message', message => {
   outputMessage(message)
+
+  chatMessages.scrollTop = chatMessages.scrollHeight
 })
 
 // Message submit
